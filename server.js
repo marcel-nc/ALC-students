656dbd170a883567aa73d8acb80a2a8fd2dc6a63
MARCEL <MARCEL>
2017-11-10 14:03:39 +0100

    restructure

--- server.js
+++ server.js
@@ -1,21 +1,64 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+// API routes
 const route = require('./routes');
 
 const app = express();
 
+// Parse data
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Use CORS
 app.use(cors());
 
+// Point static path to dist
+app.use(express.static(path.join(__dirname, '/dist')));
+
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+// Set our api routes
 app.use('/api', route);
 
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
 
 
-app.listen(3000);
-console.log('app is running')
+ // Get port from environment and store in Express.
+ 
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+
+//Create HTTP server.
+ 
+const server = http.createServer(app);
+
+
+//Listen on port
+ 
+server.listen(port, () => console.log(`API running on localhost:${port}`));
